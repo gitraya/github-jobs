3847df79577f4677e39154050a6c0e40b2fcd813
gitraya <mail.raya15@gmail.com>
2021-03-26 11:34:16 +0700

    Update

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,28 @@
 function App() {
-  return <div className="App">Github Jobs</div>;
+  return (
+    <div className="App">
+      <header>
+        <div className="App-logo">Github Jobs</div>
+      </header>
+      <main>
+        <section className="search-section">
+          <div className="search-box">
+            <form>
+              <div className="input-control">
+                <input
+                  id="search"
+                  name="search"
+                  type="text"
+                  placeholder="Title, companies, expertise or benefits"
+                />
+                <input type="submit" value="Search" />
+              </div>
+            </form>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
